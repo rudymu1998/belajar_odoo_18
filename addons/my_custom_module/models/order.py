2c762e34595e88655a6e42980343e8d1d5ce850d
Rudy <95736349+rudymu1998@users.noreply.github.com>
2026-05-26 09:55:03 +0700

    addson ordernmuber auto generate

--- addons/my_custom_module/models/order.py
+++ addons/my_custom_module/models/order.py
@@ -1,19 +1,20 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from datetime import datetime
 
 class CustomOrder(models.Model):
     _name = 'custom.order'
     _description = 'Custom Order'
 
-    name = fields.Char(string='Order Number', required=True, default)
+    name = fields.Char(string='Order Number', default='xxxxxxxxxxx', readonly= True)
     customer_name = fields.Char(string='Customer')
     order_date = fields.Date(string='Order Date')
-    amount = fields.Float(string='Total Amount')
+    amount = fields.Float(string='Total Amount' ,readonly= True)
 
     state = fields.Selection([
         ('draft', 'Draft'),
         ('confirm', 'Confirmed'),
         ('done', 'Done')
-    ], default='draft', string='Status')
+    ], default='draft', string='Status', required=True)
 
     line_ids = fields.One2many(
         'custom.order.line',
@@ -26,3 +27,14 @@ class CustomOrder(models.Model):
 
     def action_done(self):
         self.state = 'done'
+
+    @api.model
+    def create(self, vals):
+        record = super(CustomOrder, self).create(vals)
+
+        dt = datetime.today()
+        date_str = dt.strftime('%Y%m%d')
+
+        record.name = f"{date_str}-{record.id}"
+
+        return record
